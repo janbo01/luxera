a7fca673a577c695faadac5fcb120d2b0905ec37
Janbo <privomid@gmail.com>
2026-05-28 18:13:53 +0330

    fixes

--- src/components/shared/EnamadLogo.tsx
+++ src/components/shared/EnamadLogo.tsx
@@ -1,18 +1,17 @@
-const EnamadLogo = () => (
-  <a
-    referrerPolicy="origin"
-    target="_blank"
-    rel="noopener noreferrer"
-    href="https://trustseal.enamad.ir/?id=734973&Code=qPT6vUeIooyka6VFFwT1vC3rfeuN0RHg"
-  >
-    <img
+export default function EnamadLogo() {
+  return (
+    <a
       referrerPolicy="origin"
-      src="https://trustseal.enamad.ir/logo.aspx?id=734973&Code=qPT6vUeIooyka6VFFwT1vC3rfeuN0RHg"
-      alt="نماد اعتماد الکترونیکی"
-      style={{ cursor: 'pointer', height: '40px', width: 'auto' }}
-      {...{ code: 'qPT6vUeIooyka6VFFwT1vC3rfeuN0RHg' }}
-    />
-  </a>
-)
-
-export default EnamadLogo
+      target="_blank"
+      href="https://trustseal.enamad.ir/?id=734973&Code=qPT6vUeIooyka6VFFwT1vC3rfeuN0RHg"
+    >
+      <img
+        referrerPolicy="origin"
+        src="https://trustseal.enamad.ir/logo.aspx?id=734973&Code=qPT6vUeIooyka6VFFwT1vC3rfeuN0RHg"
+        alt=""
+        style={{ cursor: "pointer" }}
+        code="qPT6vUeIooyka6VFFwT1vC3rfeuN0RHg"
+      />
+    </a>
+  )
+}
